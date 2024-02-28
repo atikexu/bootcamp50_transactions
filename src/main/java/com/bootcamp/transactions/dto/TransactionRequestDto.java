99b980d5fe8e77b7a41a0949653c7491855a4c01
atikexu <alanchumpitaz@gmail.com>
2024-02-27 20:03:47 -0500

    transactions

---
+++ src/main/java/com/bootcamp/transactions/dto/TransactionRequestDto.java
@@ -0,0 +1,21 @@
+package com.bootcamp.transactions.dto;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+import lombok.Data;
+
+/**
+ * Clase de transferencia de datos para la solicitud de Transacciones
+ */
+@Data
+public class TransactionRequestDto {
+	private String id;
+	private String productType; //AHORRO, C_CORRIENTE, PLAZO_FIJO, CRE_PERSONAL, CRED_EMPRESARIAL, TAR_CRED_PERSONAL, TAR_CRED_EMPRESARIAL
+	private String productId;
+	private String customerId;
+	private String transactionType; //DEPOSITO, RETIRO, PAGO, CONSUMO
+	private Double amount;
+	private LocalDateTime transactionDate;
+	private String customerType;
+}
---
+++ src/main/java/com/bootcamp/transactions/entity/Transaction.java
@@ -0,0 +1,28 @@
+package com.bootcamp.transactions.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+/**
+ * Clase de entidad
+ */
+@Data
+@AllArgsConstructor
+@Document(collection="transaction")
+public class Transaction {
+	@Id
+	private String id;
+	private String productType;
+	private String productId;
+	private String customerId;
+	private String transactionType;
+	private Double amount;
+	private LocalDateTime transactionDate;
+	private String customerType;
+}
